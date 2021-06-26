2d0d73bb3d9c33bb73668717df7b7b83ff5a6af6
加藤原語 <31905238+XRayZen@users.noreply.github.com>
2021-06-27 01:32:23 +0900

    修正

--- src/GetHistData/Service/Util.rs
+++ src/GetHistData/Service/Util.rs
@@ -23,23 +23,21 @@ pub fn ISOStringToDateTime(isoStr: &String) -> DateTime<Utc> {
 }
 ///ISO8601形式の文字列へ変換
 pub fn ToISOString(time: &String) -> String {
-    println!("time txt is {}",&time);
+    println!("time txt is {}", &time);
     let res = UnixTimeSecToDateTime(time.parse().unwrap());
-    println!("datetime is {}",&res);
+    println!("datetime is {}", &res);
     let t = res.format("%+").to_string();
     //2017-11-02T00:00:00Z 正しいもの
     t
 }
 
-
 pub fn UnixTimeSecToDateTime(value: i64) -> DateTime<Utc> {
     //let dt = Utc.timestamp(value, 00);
-    let mut  dates=Utc::now();
-    let dt=DateTime::parse_from_str("1970/01/01 00:00:00", "%Y/%m/%d %H:%M:%S");
+    let mut dates: DateTime<FixedOffset>;
+    let dt = DateTime::parse_from_str("1970/01/01 00:00:00", "%Y/%m/%d %H:%M:%S");
     match dt {
-        Ok(date) =>dates =date.with_timezone(&Utc),
-        Err(error) =>println!("Unixtime error {}",error),
+        Ok(date) => dates = date + Duration::milliseconds(value),
+        Err(error) => println!("Unixtime error {}", error),
     }
-    let te= dates+ Duration::milliseconds(value);
-    te
+    dates.with_timezone(&Utc)
 }
